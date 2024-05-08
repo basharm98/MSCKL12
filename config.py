8072fff96c6947c56bb28602b232a2dc9156d26d
basharm98 <167342964+basharm98@users.noreply.github.com>
2024-05-08 13:16:03 +0200

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://abood:king@cluster0.rbp1cqz.
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 480))
 
 # Chat id of a group for logging bot s activities
-LOGGER_ID = int(getenv("LOGGER_ID"))
+LOGGER_ID = int(getenv("LOGGER_ID",1002036974053))
 
 # Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 6428208306))
